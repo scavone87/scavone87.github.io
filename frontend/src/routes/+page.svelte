36bd18a556200b86ad68197676a01483bd1eefbe
rocco <r.scavone@tabsrl.com>
2025-05-12 23:05:47 +0200

    Aggiornato Dockerfile e Gemfile per utilizzare Ruby 3.4 e aggiunti nuovi gem per la compatibilità. Modificato Dockerfile del frontend per utilizzare l'immagine slim di Node. Aggiornato indirizzo email di contatto e migliorata configurazione Nginx con intestazioni proxy e timeout.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -262,7 +262,7 @@
               <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
               <polyline points="22,6 12,13 2,6"></polyline>
             </svg>
-            <a href="mailto:r.scavone@tabsrl.com" class="animated-link">r.scavone@tabsrl.com</a>
+            <a href="mailto:scavone87@gmail.com" class="animated-link">scavone87@gmail.com</a>
           </div>
 
           <div class="contact-item">
